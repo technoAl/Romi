331657c04170392542099e79d7f5490ec57ba218
Alexander Sun <technoal9@gmail.com>
2021-02-24 18:57:15 -0500

    new trajectory

--- src/main/java/frc/robot/commands/autos/BasicTrajectory.java
+++ src/main/java/frc/robot/commands/autos/BasicTrajectory.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 import frc.robot.commands.autos.Trajectories;
@@ -11,11 +12,13 @@ import frc.robot.commands.autos.Trajectories;
 public class BasicTrajectory extends Trajectories {
     private static final Pose2d END_POINT = new Pose2d(0.25, 0, new Rotation2d(0));
     public static final Trajectory MOVE = TrajectoryGenerator.generateTrajectory(
+        new Pose2d(0, 0, new Rotation2d(0)),
         List.of(
-            new Pose2d(0, 0, new Rotation2d(0)), 
-            new Pose2d(0.25, 0, new Rotation2d(0)),
-            new Pose2d(0.25, 0.25, new Rotation2d(90))
-            ), 
+            new Translation2d(0.5, 0.25),
+            new Translation2d(1.0, -0.25),
+            new Translation2d(1.5, 0)
+        ),
+        new Pose2d(0.0, 0, new Rotation2d(Math.PI)),
         FORWARD_CONFIG);
     
 }
